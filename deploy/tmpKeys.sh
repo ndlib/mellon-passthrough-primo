765422eaa6341d1339818c51c89c19d992d1cc41
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-10-29 09:45:49 -0400

    Update to work with primo api

--- deploy/tmpKeys.sh
+++
@@ -1,3 +0,0 @@
-export GIT_VERSION=dev
-
-export PRIMO_API_KEY=dev
--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const fetch = require('node-fetch')
 
 exports.handler = async (event, context, lambdaCallback) => {
   try {
-    const query = event.path + "?" + Object.keys(event.queryStringParameters).map(key => key + '=' + event.queryStringParameters[key]).join('&')
-    const url = process.env.PASSTHROUGH_URL
-    let res = await fetch(url + query)
+    const query = event.path + "?" + encodeURIComponent(Object.keys(event.queryStringParameters).map(key => key + '=' + event.queryStringParameters[key]).join('&'))
+//    const url = process.env.PASSTHROUGH_URL + query
+    let url = "https://api-na.hosted.exlibrisgroup.com" + query
+
+    const requestHeaders = {
+        method: "get",
+        headers: { "Authorization": "apikey " + process.env.PRIMO_API_KEY }
+    }
+
+    let res = await fetch(url, requestHeaders )
     const body = await res.text()
+
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Content-Type": res.headers.get("Content-type"),
@@ -28,8 +36,8 @@ exports.handler = async (event, context, lambdaCallback) => {
 exports.test = () => {
   exports.handler(
     {
-      path: "/ndu_local",
-      queryStringParameters: { preview: true, query: "query" },
+      path: "/primo/v1/pnxs",
+      queryStringParameters: { inst: "NDU", search_scope: "spec_coll", q: "any,contains,book" },
     },
     {},
     (err, data) => { console.log(JSON.stringify(JSON.parse(data.body), null, 2)) }
