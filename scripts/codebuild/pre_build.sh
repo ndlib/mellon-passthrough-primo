6ee4a45e3defc90d1bae63dade39d0cb94159d2c
jgondron <jgondron@nd.edu>
2019-06-07 08:54:02 -0400

    Template name change
    The template in the blueprints has changed to primo-passthrough.yml

--- scripts/codebuild/pre_build.sh
+++ scripts/codebuild/pre_build.sh
@@ -1,7 +1,7 @@
 echo "[Pre-Build phase] `date` in `pwd`"
 
 passthrough_dir="${BLUEPRINTS_DIR}/deploy/cloudformation/"
-passthrough_yml="manifest-passthrough.yml"
+passthrough_yml="primo-passthrough.yml"
 
 echo "Copy the $passthrough_yml to this project so it can run"
 echo "${passthrough_dir}${passthrough_yml}"
